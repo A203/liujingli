72c13a294d734561b06263d6e76821b88054d225
liujingli <1013640163@qq.com>
2015-07-08 15:47:49 +0800

    firsthomework

---
+++ Day01.java
@@ -0,0 +1,17 @@
+package day0103;
+
+public class Day01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  char s='a';
+  char x='¾Í';
+  System.out.println(s);
+  System.out.println(x);
+  	 }
+	
+
+}
---
+++ Java0102.java
@@ -0,0 +1,16 @@
+package day0102;
+
+public class Java0102 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag=true;
+		System.out.println(flag);
+		boolean flag1=false;
+		System.out.println(flag1);
+	}
+
+}
